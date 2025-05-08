8085fae980fa3cea125152c20866b58dd1737786
carlossof <167620210+carlossof@users.noreply.github.com>
2025-05-08 19:58:39 +0200

    integrate registration with firebase

--- src/Registro.jsx
+++ src/Registro.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { auth } from './config/firebase.config'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+
 
 export default function Registro() {
+
+
+    const email = "carlossoftwaail@manolo.com"
+    const password = "123456"
+
+    const register = async () => {
+        const test = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(test)
+        console.log("HOLA", test.user)
+    }
+
+
+
+
     return (
-        <div>Registro</div>
+        <button onClick={register}> Registro </button>
     )
 }
